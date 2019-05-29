d3db21589be35adc7d6868a796dc193de9109520
ryan gardiner <ryan.gardiner@woodmac.com>
2019-05-29 17:04:04 +0100

    p1

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -11,6 +11,8 @@ namespace GradeBook.GradeBooks
 {
     public class BaseGradeBook
     {
+        public GradeBookType Type { get; set; }
+
         public string Name { get; set; }
         public List<Student> Students { get; set; }
 
